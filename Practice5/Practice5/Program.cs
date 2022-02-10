36aeaabd8c523f4e2a562e1ab6dc53f6027be551
21TekSavvvy <jareddarochajj@gmail.com>
2022-02-09 23:32:01 -0800

    Final

---
+++ Practice5/Practice5/Program.cs
@@ -0,0 +1,27 @@
+﻿class HeightCategory
+{
+    static void Main()
+    {
+        int inches = 70;
+        double centimeterConverted = inches * 2.54;
+        if( centimeterConverted >195)
+        {
+            System.Console.WriteLine("Abnormal height");
+        }
+        else if(centimeterConverted <=195 | centimeterConverted > 165)
+        {
+            System.Console.WriteLine("Tall");
+        }
+        else if (centimeterConverted <= 165 | centimeterConverted > 150)
+        {
+            System.Console.WriteLine("Average Height");
+        }
+        else
+        {
+            System.Console.WriteLine("Dwarf");
+        }
+
+        System.Console.ReadKey();
+    }
+
+}
